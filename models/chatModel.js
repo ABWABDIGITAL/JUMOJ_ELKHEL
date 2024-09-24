9897c56193becf57b90720c809afb3a4dadf9192
Fatma <Fatma.M.Elessawy@gmail.com>
2024-09-24 10:54:37 +0300

    update chat

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,11 +1,11 @@
 const pool = require('./config/db');
-const io = require('socket.io')(server);  // Make sure 'server' is defined correctly
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
   socket.on('chatMessage', async (messageData) => {
     console.log('Message received:', messageData);
 
-    // Emit the message to other users
+    // Emit the message to all clients in the room
     io.emit('chatMessage', messageData);
 
     // Save message to the database
@@ -14,21 +14,36 @@ io.on('connection', (socket) => {
 
       // Insert the message into the messages table
       const result = await pool.query(
-        'INSERT INTO messages (sender_id, receiver_id, text, timestamp, last_message_id, last_message_date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+        `INSERT INTO messages (room_id, sender_id, receiver_id, text, created_at, last_message_id, last_message_date) 
+         VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
         [
-          messageData.senderId,       // Sender ID
-          messageData.receiverId,     // Receiver ID
-          messageData.text,           // Message text
-          timestamp,                  // Timestamp
-          messageData.lastMessageId,  // Last message ID (if applicable)
+          messageData.roomId,        // Room ID
+          messageData.senderId,      // Sender ID
+          messageData.receiverId,    // Receiver ID
+          messageData.text,          // Message text
+          timestamp,                 // Created at timestamp
+          messageData.lastMessageId, // Last message ID (if applicable)
           messageData.lastMessageDate // Last message date (if applicable)
         ]
       );
 
-      const messageId = result.rows[0].id;  // Get the ID of the newly inserted message
-      console.log('Message inserted with ID:', messageId);
+      const insertedMessage = result.rows[0];
+      console.log('Message inserted:', insertedMessage);
+
+      // Send the inserted message back to the client
+      socket.emit('messageSaved', {
+        success: true,
+        message: 'Message sent successfully',
+        data: insertedMessage
+      });
+
     } catch (error) {
       console.error('Error inserting message:', error);
+      socket.emit('messageError', {
+        success: false,
+        message: 'Failed to send message',
+        data: error.message
+      });
     }
   });
 });

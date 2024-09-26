206f65b5e221c6ee4001e17028c49b4a67151f54
Fatma <Fatma.M.Elessawy@gmail.com>
2024-09-26 10:23:17 +0300

    updated

--- routes/myPaymentRoutes.js
+++ routes/myPaymentRoutes.js
@@ -3,35 +3,35 @@ const { createNewPayment, getPayments, getPaymentByIdController } = require('../
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const ApiError = require('../utils/ApiError');
 
+
+
+//const { uploadSingleImage } = require('../middleware/uploadImageMiddleware');
 // Multer storage configuration
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Correct file path for uploading images
-    cb(null, path.join(__dirname, '../uploads/Payments/'));
-  },
-  filename: function (req, file, cb) {
-    // Use Date.now() to ensure a unique filename
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-
-// File filter to allow only images
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    // Pass an error if the file is not an image
-    cb(new ApiError('Only image files are allowed!', 400), false);
-  }
-};
-
-// Create the Multer instance with the storage and file filter
-const upload = multer({ storage, fileFilter });
-
-// Route to create a new payment with an image upload
-router.post('/create', upload.single('image'), createNewPayment);
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../uploads/Payments/'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  });
+  
+  // File filter to allow only images
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+      cb(null, true);
+    } else {
+      cb(new ApiError('Only image files are allowed!', 400), false);
+    }
+  };
+  
+  // Multer instance
+  const upload = multer({ storage, fileFilter });
+// Route to create a new payment
+router.post('/create', upload.single("image"), createNewPayment);
 
 // Route to get all payments
 router.get('/payments', getPayments);

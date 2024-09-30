86d6275c684530accb098ca54675734c513aaba5
Fatma <Fatma.M.Elessawy@gmail.com>
2024-09-30 11:55:43 +0300

    update route

--- routes/searchByLocationRoutes.js
+++ routes/searchByLocationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const LocationSearchModel = require('./models/LocationSearchModel'); // Adjust the path accordingly
+const LocationSearchModel = require('../models/LocationSearchModel'); // Adjust the path accordingly
 
 // Search by location
 router.get('/api/search', async (req, res) => {
